5c957c477f07b21e8e6d690d515c497911002c69
Snigdha Aryakumar <saryakumar@travix.com>
2022-12-08 00:38:23 +0100

    terraform hello

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.1.0"
+  cloud {
+    hostname = "app.terraform.io"
+    organization = "terraform-controller-demo"
+
+    workspaces {
+      name = "example"
+    }
+  }
+}
+
+variable "subject" {
+   type = string
+   default = "audience"
+   description = "Subject to hello"
+}
+
+output "terraform_hello" {
+  value = "Hello, ${var.subject}!"
+}
+
